b79a53a67d919b4aebb135c506d7fc0736ab6c01
Neil Montgomery <neilm@mie.utoronto.ca>
2016-01-14 22:59:22 -0500

    corrected

--- srs.R
+++ srs.R
@@ -1,10 +1,10 @@
 values <- 1:10
 
 # Simple random sampling of size 5
-sample(values, 5, replace = TRUE)
+sample(values, size = 5, replace = FALSE)
 
 # Simulated probability of each element
-prop.table(table(replicate(100000, sample(values, 5, replace = TRUE))))
+prop.table(table(replicate(100000, sample(values, 5, replace = FALSE))))
 
 # Not simple random sampling. Flip a coin. Heads: pick items 1, 3, 5, 7, 9.
 # Tails: pick items 2, 4, 6, 8, 10
